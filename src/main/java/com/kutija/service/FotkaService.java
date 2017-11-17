614cc6cab6156e329baf731b4eb5091591aa4f6c
Damir Hadnagyev <33582312+hadnagyev@users.noreply.github.com>
2017-11-17 11:09:54 +0000

    clean up again

--- src/main/java/com/kutija/service/FotkaService.java
+++ src/main/java/com/kutija/service/FotkaService.java
@@ -1,7 +1,6 @@
 package com.kutija.service;
 
 import java.util.List;
-
 import com.kutija.model.Fotka;
 
 public interface FotkaService {
@@ -12,6 +11,7 @@ public interface FotkaService {
 	Fotka save(Fotka fotka);
 	
 	Fotka findNextFotka(Long id);
+	
 	Fotka findPreviousFotka(Long id);
 	
 	Fotka delete(Long id);
